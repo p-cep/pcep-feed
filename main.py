1946f084f4b922de6eff4ff0427a46c9c50a5189
WindowsVistaisCool <kwrush024@pccsk12.com>
2021-09-13 20:36:22 -0400

    epicmxnia

--- main.py
+++ main.py
@@ -23,8 +23,13 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.author.bot: return
-    if f"{client.user.id}" in message.content:
+    if f"{client.user.id}" in message.content and 'can i go on a date' not in message.content:
         await message.reply(content="what the fuck do you want bitch")
+    elif f"{client.user.id}" in message.content and 'can i go on a date' in message.content:
+        if str(message.author.id) not in config['owner_ids']:
+            await message.reply(content="srry i have boyfriend")
+            return
+        await message.reply(content="yes bb ofc i will go on date w/ u :kissing_heart:")
     await client.process_commands(message)
 
 @client.command()
