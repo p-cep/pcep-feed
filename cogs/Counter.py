eb16110a25ea9451d6a40c5691cc5e9747441a71
Alok Swaminathan <alokswami2007@gmail.com>
2021-09-29 06:10:04 -0400

    updating stuff
    code is pointless tho cuz i didnt realize this bot was gonna count in both channels but whatever

---
+++ cogs/Counter.py
@@ -0,0 +1,30 @@
+import asyncio
+import discord
+from discord.ext import commands
+from discord.ext.commands.core import command
+from cogs import util
+import asyncio
+import json
+
+class Counter(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def on_raw_reaction_add(self, payload):
+        with open ("../config.json","r") as config:
+            normChannel = config["countingNormalChannelID"]
+            seriousChannel = config["countingSeriousChannelID"]
+            roleID = config["countingRoleID"]
+            ownerIDs = config["owner_ids"]
+        if (payload.emoji.name == "white_check_mark" or "ballot_box_with_check") and payload.user_id==510016054391734273 and payload.message.channel.id == normChannel:
+            with open ("../countingData.json","r") as file:
+                countingDict = json.load(file)
+
+            if payload.message.user_id in countingDict.keys():
+                countingDict[payload.message.user_id] = countingDict[payload.message.user_id] + 1
+            else:
+                countingDict[payload.message.user_id] = 1
+            if countingDict[payload.message.user_id] == 100:
+                await payload.message.user_id.add_roles()
+                
