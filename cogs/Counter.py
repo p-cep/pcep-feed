3608fefe8bf97d0b446826e7883408ec1e54a898
Kyle Rush <57149988+WindowsVistaisCool@users.noreply.github.com>
2021-09-29 10:07:09 -0400

    fix alok's mistakes

--- cogs/Counter.py
+++ cogs/Counter.py
@@ -1,9 +1,7 @@
+import asyncio
 import discord
 from discord.ext import commands
-from discord.ext.commands.core import command
 from cogs import util
-import asyncio
-import json
 
 class Counter(commands.Cog):
     def __init__(self,bot):
@@ -11,14 +9,13 @@ class Counter(commands.Cog):
     
     @commands.Cog.listener()
     async def on_raw_reaction_add(self, payload):
-        with open ("../config.json","r") as config:
-            normChannel = config["countingNormalChannelID"]
-            seriousChannel = config["countingSeriousChannelID"]
-            roleID = config["countingRoleID"]
-            ownerIDs = config["owner_ids"]
-        if (payload.emoji.name == "white_check_mark" or "ballot_box_with_check") and payload.user_id==510016054391734273 and payload.message.channel.id == normChannel:
-            with open ("../countingData.json","r") as file:
-                countingDict = json.load(file)
+        config = util.store('counterConfig.json', None, True) # none = no key, true = read
+        normChannel = config["countingNormalChannelID"]
+        seriousChannel = config["countingSeriousChannelID"]
+        roleID = config["countingRoleID"]
+        ownerIDs = store('config.json', 'owner_ids', True)
+        if (payload.emoji == "✅" or "☑️") and payload.user_id==510016054391734273 and payload.message.channel.id == normChannel:
+            countingDict = store("countingData.json", None, True)
 
             if payload.message.user_id in countingDict.keys():
                 countingDict[payload.message.user_id] = countingDict[payload.message.user_id] + 1
@@ -27,3 +24,5 @@ class Counter(commands.Cog):
             if countingDict[payload.message.user_id] == 100:
                 await payload.message.user_id.add_roles()
                 
+def setup(bot):
+    bot.add_cog(Counter(bot))
